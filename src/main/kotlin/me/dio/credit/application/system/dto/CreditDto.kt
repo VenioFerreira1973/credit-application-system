25e1f9cccc6134a38c13bee913e83fb7b4acac9d
VenioFerreira1973 <venioferreira@gmail.com>
2023-05-30 00:35:52 -0300

    Atualizacao arquivos finalização do desafio de testes usando mockk

--- src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
+import me.dio.credit.application.system.anotations.FutureInThreeMonths
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.entity.Customer
 import java.math.BigDecimal
@@ -13,9 +14,9 @@ import java.util.*
 data class CreditDto(
 
     @field:NotNull(message = "O valor é obrigatório") val creditValue: BigDecimal,
-    @field:Future(message = "A data informa é inválida") val dayFirstInstallment: LocalDate,
+    @field:FutureInThreeMonths(message = "A data informada deve ser exatamente 3 meses no futuro") val dayFirstInstallment: LocalDate,
     @field:Min(value = 1, message = "O numero de parcelas deve ser maior que 1") @field:Max(
-        value = 12,
+        value = 48,
         message = "O numero de parcelas deve ser menor que 12"
     ) val numberOfInstallments: Int,
     @field:NotNull(message = "O id do cliente é obrigatório") var customerId: Long,
